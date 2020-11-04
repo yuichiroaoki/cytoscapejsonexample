92f442bd646642063e7e4e415c2f0ddcfbc103f0
yuichiroaoki <yuichiroaoki27@gmail.com>
2020-11-04 13:37:14 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import responder
+import json
+
+api = responder.API(templates_dir='./static')
+
+#ホームにアクセスするとReactのviewが表示される
+@api.route("/")
+def index(req, resp):
+    resp.html= api.template('index.html')
+
+
+@api.route("/bigGraph.json")
+def bigGraph(req, resp):
+    with open('static/bigGraph.json') as f:
+        df=json.load(f)
+
+    resp.media= df
+
+if __name__ == '__main__':
+    api.run()
