558d18d3bfa2b8bc81f635d4178d3521b1202942
yuichiroaoki <yuichiroaoki27@gmail.com>
2020-11-13 16:14:28 +0900

    add data

--- main.py
+++ main.py
@@ -8,27 +8,13 @@ api = responder.API(templates_dir='./static')
 def index(req, resp):
     resp.html= api.template('index.html')
 
-
-@api.route("/bigGraph.json")
-def bigGraph(req, resp):
-    with open('static/bigGraph.json') as f:
-        df=json.load(f)
-
-    resp.media= df
-
-@api.route("/new_position.json")
-def new_position(req, resp):
-    with open('static/new_position.json') as f:
+@api.route("/{file}/json")
+def json_file(req, resp, *, file):
+    with open('static/'+ file + '.json') as f:
         data=json.load(f)
 
     resp.media= data
 
-@api.route("/full_data.json")
-def full_data(req, resp):
-    with open('static/full_data.json') as f:
-        data=json.load(f)
-
-    resp.media= data
 
 if __name__ == '__main__':
     api.run()
